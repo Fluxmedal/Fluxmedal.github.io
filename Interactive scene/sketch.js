9bf4e59eb6fd392f1003838c55931fc14e2b2846
Fluxmedal <canadian20022@gmail.com>
2020-02-10 14:10:40 -0600

    stuff

---
+++ Interactive scene/sketch.js
@@ -0,0 +1,34 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  
+
+
+}
+
+
+
+
+function draw(stickman)//stickman
+{
+  background(0,100,175 );
+  fill(255,255,255);
+  ellipse(500,500,200,200); //add mouseX + number and mouseY + number
+  fill (0,255,0);
+  ellipse(466,466,20,20);
+  ellipse(544,466,20,20);
+  line(498,600,498,794);
+  strokeWeight(4);
+  line(497,794,402,940);
+  line(497,794,577,940);
+  
+  
+}
