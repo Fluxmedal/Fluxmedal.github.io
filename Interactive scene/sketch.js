907ee3ee4e1fda86e554fa82146e1e2237be030f
Fluxmedal <canadian20022@gmail.com>
2020-02-25 13:01:47 -0600

    Update sketch.js

--- Interactive scene/sketch.js
+++ Interactive scene/sketch.js
@@ -5,6 +5,10 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let r2,g2,b2;
+r2 = 255;
+g2 = 255;
+b2 = 0;
 
 
 let r,g,b;
@@ -12,11 +16,12 @@ r = 0;
 g = 255;
 b = 0;
 
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let eye = 0;
 
   
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  text("Jason Rendle", 1000, 1000, 1100, 1100);
 
 
 }
@@ -24,9 +29,12 @@ function setup() {
 
 
 
-function draw(stickman)//stickman
-{
+function draw(stickman){//stickman{
+  
   background(0,100,175 );
+  fill(255,255,255);
+  fill (r2,g2,b2);
+  ellipse(2258,173,200,200);
   fill(255,255,255);
   ellipse(mouseX+250,mouseY+200,200,200); //add mouseX + number and mouseY + number
   fill (r,g,b);
@@ -39,4 +47,43 @@ function draw(stickman)//stickman
   line(mouseX+248,mouseY+328,mouseX+196,mouseY+352);
   line(mouseX+248,mouseY+328,mouseX+296,mouseY+353);
   line(mouseX+227,mouseY+272,mouseX+277,mouseY+266);
+  rect(0,1000,3080,1000);
+  
+  
+}
+
+
+function keyPressed(){
+  if ( keyCode === 38){
+    
+    r = 0;
+    g = 255;
+    b = 0;
+    eye = eye +1;
+    
+  }
+  if (keyPressed){
+    if (eye > 3){
+      eye = 0;
+    }
+    if (keyCode === 38){
+      if (eye === 0){
+        r = 0;
+        g = 255;
+        b = 0;
+        eye = eye ++;
+      }
+      if (eye === 1){
+        r = 255;
+        g = 0;
+        b = 0;
+        eye = eye ++;
+      }
+      if (eye === 2){
+        r = 0;
+        g = 0;
+        b = 255;                     
+      }         
+    }
+  }
 }
